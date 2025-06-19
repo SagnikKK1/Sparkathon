15187f49adc1296e5b46e451ea47e96d8f64e716
Aditya Singh <asingh772004@gmail.com>
2025-06-20 02:06:01 +0530

    Added Dashboard to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/header";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
     const [theme, setTheme] = useState<"light" | "dark">(() => {
@@ -16,17 +17,11 @@ function App() {
     const toggleTheme = () => {
         setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
     };
+
     return (
         <div className="App">
-            <Header toggleTheme={toggleTheme} currentTheme={theme}/>
-            
-            {/* <button */}
-                {/* onClick={toggleTheme} */}
-                {/* className="theme-toggle-button" */}
-            {/* > */}
-                {/* Switch to {theme === "light" ? "Dark" : "Light"} Mode */}
-            {/* </button> */}
-
+            <Header toggleTheme={toggleTheme} currentTheme={theme} />
+            <Dashboard />
         </div>
     );
 }
