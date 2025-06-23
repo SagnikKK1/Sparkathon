88198f285f7ba8070b12bb4ed8a6d1c245cd60d8
Aditya Singh <asingh772004@gmail.com>
2025-06-24 00:55:50 +0530

    Added more colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,6 +9,9 @@ module.exports = {
     extend: {
       colors: {
         background: "#011627",      // blackish
+        "background-dark": "#0e111a",
+        card: "#ffffff",
+        "card-dark": "#181d2b",
         positive: "#23F0C7",        // greenish
         yellowish: "#f7b32b",       // yellowish (headings)
         negative: "#f72c25",        // red (negative)
