00e70be7c68c28906280a21960f02cdc6cc4089d
Aditya Singh <asingh772004@gmail.com>
2025-07-01 23:58:55 +0530

    Created virality.tsx

---
+++ frontend/src/components/dashboard/virality.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../components_css/dashboard_css/virality_score.css';
+
+export const DashboardViralityScore: React.FC = () => {
+  // Example data
+  const viralityScore = 86;
+  const searches = 1243;
+
+  return (
+    <div className="virality-score-tile">
+      <div className="virality-score-number">{viralityScore}</div>
+      <div className="virality-score-label">Virality Score</div>
+      <div className="virality-score-searches">{searches} searches</div>
+    </div>
+  );
+};
